587a1aa1a32c65ae8f2dc42e11daab4d4628b9fd
NICD <tealovingbrit@gmail.com>
2016-03-14 21:04:24 +0000

    Added QueueApi for adding emails to queues.

---
+++ src/Api/QueueApi.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nic
+ * Date: 14/03/2016
+ * Time: 20:47
+ */
+
+namespace Nybbl\Api;
+
+use Nybbl\Api\Api;
+
+class QueueApi extends Api
+{
+    /**
+     * QueueApi constructor.
+     * @param string $endpoint
+     * @param string $accessToken
+     */
+    public function __construct($endpoint, $accessToken)
+    {
+        parent::__construct($endpoint, $accessToken);
+    }
+
+    /**
+     * Adds data to the email queue
+     *
+     * @param array $data
+     * @return array
+     */
+    public function createNewEmailQueue($data)
+    {
+        $route = '/queue/email';
+        return $this->doApiRequest($route, 'POST', $data);
+    }
+}
